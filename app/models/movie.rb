930f6f0340965824fe3314aea272c1d2852e9b4a
Zachary Farrell <zfarrell0507@gmail.com>
2022-09-13 14:36:09 -0400

    commit tree dfee78b222f2a467f7edc1033456559b4d969e98

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,35 @@
 class Movie < ActiveRecord::Base
-
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
+    def self.first_movie
+        Movie.first
+    end
+    def self.last_movie
+        Movie.last
+    end
+    def self.movie_count
+        Movie.count
+    end
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+    def self.find_movie_with_attributes(attr)
+        Movie.find_by(attr)
+    end
+    def self.find_movies_after_2002
+        Movie.where(release_date: (2003)..)
+    end
+    def update_with_attributes(attr)
+        self.update(attr)
+    end
+    def self.update_all_titles(title)
+        Movie.update(title: title)
+    end
+    def self.delete_by_id(id)
+        Movie.find(id).destroy
+    end
+    def self.delete_all_movies
+        Movie.destroy_all
+    end
 end
